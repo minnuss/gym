5e31cd65cebafc3a5e03d04864c5cf275623e80c
minnuss <ponjevic.goran@hotmail.com>
2021-03-31 20:50:41 +0200

    close hamb menu on link click

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,21 @@
 const header = document.querySelector('.header');
 const hambBtn = document.querySelector('.header__menu');
 const overlay = document.querySelector('.header__overlay');
+const links = document.querySelectorAll('.header__nav--links__item');
 
 
 hambBtn.addEventListener('click', () => {
     header.classList.toggle('open');
     hambBtn.classList.toggle('open');
     overlay.classList.toggle('open');
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        hambBtn.click();
+    })
+})
+
+overlay.addEventListener('click', () => {
+    hambBtn.click();
 })
